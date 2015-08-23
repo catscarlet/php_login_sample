f2948753c51d9c7ded2736e0042a5bdcdcf6ce11
catscarlet <i@catscarlet.com>
2015-08-24 00:25:13 +0800

    add session and cookie

---
+++ admin.php
@@ -0,0 +1,24 @@
+<?php
+
+echo '<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <title>Page for SESSION and COOKIE verification</title>
+</head>
+';
+
+if ($_SESSION['username'] != 'root' || $_SESSION['userpwd'] != 'admin') {
+    echo 'SESSION mismatch!';
+} else {
+    echo 'SESSION match!';
+}
+
+echo '<br>';
+
+if ($_COOKIE['Biscuit'] != 'Biscuit') {
+    echo 'COOKIE not match!';
+} else {
+    echo 'COOKIE match!';
+}
--- login.php
+++ login.php
@@ -1,19 +1,28 @@
+
 <?php
-$username=$_POST['username'];
-$userpwd=$_POST['userpwd'];
+session_start();
+$_SESSION['username'] = null;
+$_SESSION['userpwd'] = null;
+$_SESSION['username'] = $_POST['username'];
+$_SESSION['userpwd'] = $_POST['userpwd'];
 
+echo '<!DOCTYPE html>
+<html lang="en">
 
+<head>
+  <meta charset="UTF-8">
+  <title>Page for information verification</title>
+</head>
+';
 
-
-if ($username!='root'||$userpwd!='admin')
-{
-  echo 'Your information is wrong , please check .';
+if ($_SESSION['username'] != 'root' || $_SESSION['userpwd'] != 'admin') {
+    echo 'Your information is wrong , please check .';
+} else {
+    setcookie('Biscuit', 'Biscuit');
+    echo 'Login Successfully';
+    echo '<p>
+    <a href="admin.php">Click to access to next page .</a>
+  </p>';
 }
- else {
-  echo 'Login Successfully';
-  }
-
-
-
 
  ?>
