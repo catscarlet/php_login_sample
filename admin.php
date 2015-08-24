db01e131d66d47bf84ba295090639213b47e433d
catscarlet <i@catscarlet.com>
2015-08-24 15:56:26 +0800

    add some echo

--- admin.php
+++ admin.php
@@ -9,8 +9,14 @@ echo '<!DOCTYPE html>
 </head>
 ';
 
+
+echo '$_SESSION[\'username\'] is '.$_SESSION['username'].'<br>';
+echo '$_SESSION[\'userpwd\'] is '.$_SESSION['userpwd'].'<br>';
+echo '$_COOKIE[\'Biscuit\'] is '.$_COOKIE['Biscuit'].'<br><br>';
+
+
 if ($_SESSION['username'] != 'root' || $_SESSION['userpwd'] != 'admin') {
-    echo 'SESSION mismatch!';
+    echo 'SESSION mismatch!!!';
 } else {
     echo 'SESSION match!';
 }
@@ -18,7 +24,7 @@ if ($_SESSION['username'] != 'root' || $_SESSION['userpwd'] != 'admin') {
 echo '<br>';
 
 if ($_COOKIE['Biscuit'] != 'Biscuit') {
-    echo 'COOKIE not match!';
+    echo 'COOKIE mismatch!!!';
 } else {
     echo 'COOKIE match!';
 }
