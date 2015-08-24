d5f182839cc3444b68e18afcb4148d9198bf9e65
catscarlet <i@catscarlet.com>
2015-08-24 16:37:48 +0800

    new function verify($username, $userpwd) and sql .

--- login.php
+++ login.php
@@ -6,6 +6,21 @@ $_SESSION['userpwd'] = null;
 $_SESSION['username'] = $_POST['username'];
 $_SESSION['userpwd'] = $_POST['userpwd'];
 
+/* The name of the database */
+define('DB_NAME', 'php_login_sample');
+/* MySQL database username */
+define('DB_USER', 'test');
+/* MySQL database password */
+define('DB_PASSWORD', 'test');
+/* MySQL database table */
+define('DB_TABLE', 'userlist');
+
+$con = mysql_connect('localhost', constant('DB_USER'), constant('DB_PASSWORD'));
+if (!$con) {
+    die('Could not connect: '.mysql_error());
+}
+mysql_select_db(constant('DB_NAME'), $con);
+
 echo '<!DOCTYPE html>
 <html lang="en">
 
@@ -15,7 +30,7 @@ echo '<!DOCTYPE html>
 </head>
 ';
 
-if ($_SESSION['username'] != 'root' || $_SESSION['userpwd'] != 'admin') {
+if (!verify($_SESSION['username'], $_SESSION['userpwd'])) {
     echo 'Your information is wrong , please check .';
 } else {
     setcookie('Biscuit', 'Biscuit');
@@ -23,6 +38,24 @@ if ($_SESSION['username'] != 'root' || $_SESSION['userpwd'] != 'admin') {
     echo '<p>
     <a href="admin.php">Click to access to next page .</a>
   </p>';
+}
+
+mysql_close($con);
+
+function verify($username, $userpwd)
+{
+    $sql = 'select * from '.constant('DB_TABLE')." where username = '".$username."'";
+    $result = mysql_query($sql);
+    while ($result && $row = mysql_fetch_array($result)) {
+        if ($row['userpwd'] == $userpwd) {
+            return true;
+        } else {
+            echo 'The Passord of '.$username.' is '.$row['userpwd'].' . not '.$userpwd.' .<br>';
+            return false;
+        }
+    }
+    echo 'User '.$username.' not found .<br>';
+    return false;
 }
 
  ?>
